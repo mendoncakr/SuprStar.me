ba962dab9f7d1a58c7690f6c02e8f533b61a4469
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-29 01:37:35 -0400

    Found missing )

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -3,58 +3,57 @@ var polling = function(){
     setTimeout(Comment.updateQueue, 5000);
     setTimeout(Comment.updateComments, 5000);
   };
-});
 
-var Comment = {
-  updateComments: function(){
-    var latestCommentTime = {time: $(".comment li:last-child").attr("data-time")};
-    $.ajax({
-      url: "/retrieve_comments",
-      method: "GET",
-      dataType: "jsonp",
-      data: latestCommentTime,
-      crossDomain: true
-    }).success(function(response){
-      for (var i=0; i < response.content.length; i++) {
-        var time = response.content[i].obj.created_at;
-        var time_in_seconds = seconds(time);
-        var appendLi = $('.comment').append('<li data-time="'+ time_in_seconds +'">' + response.content[i].obj.content + response.content[i].name + '</li>').fadeIn();
-        if($(".comment li").size() >= 5) {
-          $(".comment li:first-child").slideUp("slow").remove();
+  var Comment = {
+    updateComments: function(){
+      var latestCommentTime = {time: $(".comment li:last-child").attr("data-time")};
+      $.ajax({
+        url: "/retrieve_comments",
+        method: "GET",
+        dataType: "jsonp",
+        data: latestCommentTime,
+        crossDomain: true
+      }).success(function(response){
+        for (var i=0; i < response.content.length; i++) {
+          var time = response.content[i].obj.created_at;
+          var time_in_seconds = seconds(time);
+          var appendLi = $('.comment').append('<li data-time="'+ time_in_seconds +'">' + response.content[i].obj.content + response.content[i].name + '</li>').fadeIn();
+          if($(".comment li").size() >= 5) {
+            $(".comment li:first-child").slideUp("slow").remove();
+          }
+          appendLi;
+        };
+      }).fail(function(response){
+        console.log("No Comments yet");
+      });
+    },
+
+    updateQueue: function(){
+      $.ajax({
+        url: "/retrieve_queue",
+        method: "GET",
+        dataType: "jsonp",
+        crossDomain: true
+      }).success(function(response){
+        for (var i=0; i < response.queue.length; i++) {
+          var comment = '<li>' + response.queue[i].name + '</li>';
+          if ($('.queue li:contains("'+ response.queue[i].name +'")').length < 1) {
+            $('.queue').append(comment);
+          }
         }
-        appendLi;
-      };
-    }).fail(function(response){
-      console.log("No Comments yet");
-    });
-  },
+      }).fail(function(response){
+        console.log("FAIL");
+      });
+    }
+  };
 
-  updateQueue: function(){
-    $.ajax({
-      url: "/retrieve_queue",
-      method: "GET",
-      dataType: "jsonp",
-      crossDomain: true
-    }).success(function(response){
-      for (var i=0; i < response.queue.length; i++) {
-        var comment = '<li>' + response.queue[i].name + '</li>';
-        if ($('.queue li:contains("'+ response.queue[i].name +'")').length < 1) {
-          $('.queue').append(comment);
-        }
-      }
-    }).fail(function(response){
-      console.log("FAIL");
-    });
-  }
-};
+  var _runPolling = function() {
+    polling();
+  };
 
-var _runPolling = function() {
-  polling();
-};
-
-$(document).ready(function(){
- $("#start").on("click", (function(e){
-  e.preventDefault();
-  _runPolling();
-});
+  $(document).ready(function(){
+   $("#start").on("click", (function(e){
+    e.preventDefault();
+    _runPolling();
+  });
 });
